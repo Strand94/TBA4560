2f271e2302bfb3661724f93d493502ec859e6f20
Strand94 <Andreasstrand94@gmail.com>
2020-10-16 18:38:38 +0200

    Added simple form to eddy page

--- TBA4560_django/settings.py
+++ TBA4560_django/settings.py
@@ -35,6 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Packages
+    'widget_tweaks',
     # My apps
     'apps.staticpages',
     'apps.eddies',
--- apps/eddies/views.py
+++ apps/eddies/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .forms import SearchAreaForm
 #from apps.eddies.models import MODEL
 
 
 def eddies(request):
-    return render(request, "eddies/eddies.html", {},)
+    if request.method == 'POST':
+        form = SearchAreaForm(request.POST)
+        if form.is_valid():
+            pass  # does nothing, just trigger the validation
+    else:
+        form = SearchAreaForm()
+    return render(request, "eddies/eddies.html", {'form': form})
