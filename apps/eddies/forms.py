642fdef870b3df3375138896914ed5bbfc6c6e3a
Strand94 <Andreasstrand94@gmail.com>
2020-11-02 07:50:59 +0100

    user defined grid is now read views

--- apps/eddies/forms.py
+++ apps/eddies/forms.py
@@ -6,6 +6,8 @@ from django.contrib.gis import forms
 
 class SearchAreaForm(forms.Form):
     grid = forms.PolygonField(widget=LeafletWidget(), required=False)
+    coordinate_a = forms.CharField(required=False)
+    coordinate_b = forms.CharField(required=False)
     start_time = forms.DateTimeField(widget=DateTimePickerInput().start_of('event days'))
     end_time = forms.DateTimeField(widget=DateTimePickerInput().end_of('event days'))
 
--- apps/eddies/views.py
+++ apps/eddies/views.py
@@ -11,19 +11,22 @@ def eddies(request):
         form = SearchAreaForm(request.POST)
         if form.is_valid():
             # Collect and order datetime objects.
+            coordinate_a = form.cleaned_data['coordinate_a']
+            coordinate_b = form.cleaned_data['coordinate_b']
+            latitudes = [coordinate_a.split(",")[0],coordinate_b.split(",")[0]]
+            longitudes = [coordinate_a.split(",")[1],coordinate_b.split(",")[1]]
+            print(latitudes)
+            print(longitudes)
+
             start = form.cleaned_data['start_time']
             end = form.cleaned_data['end_time']
             grid = form.cleaned_data['grid']
             selected_dates = [start + timedelta(days=x) for x in range(0, (end.date() - start.date()).days)]
             selected_dates.append(end)
-            print("form valid")
-            print(grid)
 
-            print(selected_dates[0])
             for date in selected_dates:
                 for satellite_number in range(1, 9):
                     url = generate_url(date, satellite_number)
-                    print(url)
 
     else:
         form = SearchAreaForm()
